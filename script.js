de03b485ddca6314a25538e1a645bff8d33bcb66
M.Thanusha Muthu <m.thanushamuthu@gmail.com>
2026-06-15 10:50:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const form =
+document.getElementById("contactForm");
+
+form.addEventListener(
+"submit",
+async function(e){
+
+e.preventDefault();
+
+const data = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value
+};
+try{
+
+await fetch(
+"https://script.google.com/macros/s/AKfycbxuVev9vpx6Q30NKxUVoFiEHHEUcz4hGHV56fE_Urn1ckbfLQ07EY2eBgKZoVOCYdd7YQ/exec",
+{
+method:"POST",
+body:JSON.stringify(data)
+}
+);
+
+document.getElementById(
+"success"
+).innerText =
+"Form Submitted Successfully!";
+
+form.reset();
+
+}
+catch(error){
+
+alert(
+"Error submitting form"
+);
+
+}
+
+});
+//https://script.google.com/macros/s/AKfycbxuVev9vpx6Q30NKxUVoFiEHHEUcz4hGHV56fE_Urn1ckbfLQ07EY2eBgKZoVOCYdd7YQ/exec//
